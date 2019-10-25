a6202fc09f100bb364cc37338d1448607fe89a5a
Jeremy Trimble <devjeremyt@gmail.comgit config --global user.name Jeremy>
2019-10-24 21:49:36 -0400

    Added camp description and show page

--- app.js
+++ app.js
@@ -4,28 +4,20 @@ const express = require('express'),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/yelp_camp', {useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost:27017/yelp_camp', {useNewUrlParser: true, useUnifiedTopology: true });
 
 
 // Schema Setup
 
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
 
-Campground.create(
-    {name: "Salmon Creek", image:"https://farm5.staticflickr.com/4191/34533125526_716102b23f_m.jpg"},
-    function(err, campground){
-        if(err){
-            console.log(err);
-        } else{
-            console.log(campground.name + " was create.");
-        }
-    }
-);
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
@@ -39,7 +31,7 @@ app.get('/campgrounds',(req,res)=>{
          if(err){
              console.log(err);
          } else{
-            res.render('campgrounds', {campgrounds : campgrounds});
+            res.render('index', {campgrounds : campgrounds});
          }
      });
 });
@@ -48,10 +40,21 @@ app.get('/campgrounds/new', (req, res)=>{
         res.render('new.ejs');
 });
 
+app.get("/campgrounds/:id", (req,res)=>{
+    Campground.findById(req.params.id, (err, foundCampground)=>{
+        if(err){
+            console.log(err);
+        } else {
+            res.render('show', {campground: foundCampground});
+        }
+    });
+});
+
 app.post('/campgrounds', (req,res)=>{
     let name = req.body.name;
     let image = req.body.image;
-    let campground = {name: name, image: image};
+    let description = req.body.description;
+    let campground = {name: name, image: image, description: description};
     Campground.create(campground, (err,campground)=>{
         if(err){
             console.log(err);
